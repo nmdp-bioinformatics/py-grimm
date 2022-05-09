2685528d5d2b095d08f6eefaf6f88c3fade6fb9b
pbashyal-nmdp <pbashyal@nmdp.org>
2022-05-09 09:32:05 -0500

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -64,6 +64,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/nmdp-bioinformatics/gram",
-    version="0.0.2",
+    version="0.0.3",
     zip_safe=False,
 )
